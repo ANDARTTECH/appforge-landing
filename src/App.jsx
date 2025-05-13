d863ceb866b46f078311a3574ecb0b80ee137f02
ANDART <info@andart.tech>
2025-05-14 00:40:46 +0300

    feat: hero section V2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-violet-600 text-white">
-      <h1 className="text-4xl font-bold">Привет, Tailwind работает!</h1>
+      <h1 className="text-4xl font-bold">Hero section V2 🚀</h1>
     </div>
   );
 }
